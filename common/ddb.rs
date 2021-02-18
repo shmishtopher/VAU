aaad0f724b15d8e30589d3776e5949782f922812
Shmish <shmish90@gmail.com>
2021-02-17 21:52:11 -0500

    Added SND file header

--- common/ddb.rs
+++ common/ddb.rs
@@ -48,3 +48,37 @@ impl<'a> Iterator for Samples<'a> {
         }
     }
 }
+
+
+pub struct SNDHeader {
+    pub magic_bytes: u32,
+    pub file_size: u32,
+    pub frame_rate: u32,
+    pub channels: u16,
+    pub index: u32,
+}
+
+
+impl From<&[u8]> for SNDHeader {
+    fn from(data: &[u8]) -> Self {
+        let mut magic_bytes = [0; 4];
+        let mut file_size = [0; 4];
+        let mut frame_rate = [0; 4];
+        let mut channels = [0; 2];
+        let mut index = [0; 4];
+
+        magic_bytes.copy_from_slice(&data[0..4]);
+        file_size.copy_from_slice(&data[4..8]);
+        frame_rate.copy_from_slice(&data[8..12]);
+        channels.copy_from_slice(&data[12..14]);
+        index.copy_from_slice(&data[14..18]);
+
+        SNDHeader {
+            magic_bytes: u32::from_be_bytes(magic_bytes),
+            file_size: u32::from_le_bytes(file_size),
+            frame_rate: u32::from_le_bytes(frame_rate),
+            channels: u16::from_le_bytes(channels),
+            index: u32::from_le_bytes(index),
+        }
+    }
+}
