3e181460501103568fb1bfbfce295323b5464af2
Shmish <shmish90@gmail.com>
2021-02-25 13:50:33 -0500

    Added gui assets

--- gui/main.rs
+++ gui/main.rs
@@ -12,6 +12,86 @@
 //! limitations under the License.
 
 
-fn main() {
+use rust_embed::RustEmbed;
+use actix_web::rt::System;
+use actix_web::web;
+use actix_web::App;
+use actix_web::HttpResponse;
+use actix_web::HttpServer;
+use std::sync::mpsc;
+use std::thread;
+use web_view::Content;
+use mime_guess::from_path;
 
+
+#[derive(RustEmbed)]
+#[folder = "gui/assets"]
+struct Asset;
+
+
+fn file(path: &str) -> HttpResponse {
+    if let Some(content) = Asset::get(path) {
+        let body = content.into_owned();
+        HttpResponse::Ok()
+            .content_type(from_path(path).first_or_octet_stream().as_ref())
+            .body(body)
+    }
+    else {
+        HttpResponse::NotFound().body("404 - Not Found")
+    }
+}
+
+
+#[actix_web::main]
+async fn main() {
+    // Cross-thread channels
+    let (server_tx, server_rx) = mpsc::channel();
+    let (address_tx, address_rx) = mpsc::channel();
+
+    // Spin up webserver in a seperate thread
+    thread::spawn(move || {
+        let system = System::new("http_server");
+        let server = HttpServer::new(|| {
+            App::new()
+                .route("/", web::get().to(|| file("index.html")))
+                .route("/styles/reset.css", web::get().to(|| file("styles/reset.css")))
+                .route("/styles/layout.css", web::get().to(|| file("styles/layout.css")))
+                .route("/styles/components.css", web::get().to(|| file("styles/components.css")))
+                .route("/styles/palette.css", web::get().to(|| file("styles/palette.css")))
+                .route("/scripts/ui.js", web::get().to(|| file("scripts/ui.js")))
+                .route("/scripts/rpc.js", web::get().to(|| file("scripts/rpc.js")))
+                .route("/media/topography.svg", web::get().to(|| file("media/topography.svg")))
+        })
+        .bind("127.0.0.1:0")
+        .unwrap();
+
+        let address = server.addrs().first().unwrap().to_string();
+        let server = server.run();
+        
+        server_tx.send(server).unwrap();
+        address_tx.send(address).unwrap();
+
+        system.run().unwrap();
+    });
+
+    // Pull port and server from thread
+    let server = server_rx.recv().unwrap();
+    let address = address_rx.recv().unwrap();
+
+    println!("{}", address);
+
+    // Setup webview
+    web_view::builder()
+        .debug(true)
+        .title("VAU")
+        .size(1200, 600)
+        .resizable(false)
+        .content(Content::Url(format!("http://{}", address)))
+        .user_data(())
+        .invoke_handler(|_, _| Ok(()))
+        .run()
+        .unwrap();
+
+    // Shut down the webserver
+    server.stop(true).await;
 }
